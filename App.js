91829e71b6064e834abb876603391a70f229a17c
eunne2 <ehye518@gmail.com>
2023-02-21 14:09:35 +0900

    #2.6 Styles (최종)

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
 
 const {width:SCREEN_WIDTH} = Dimensions.get('window');
 
-
 export default function App() {
   return (
     <View style={styles.container}>

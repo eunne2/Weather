fedfcb3ac668ba0db0250123b6cfc5b08d65db50
eunne2 <ehye518@gmail.com>
2023-02-22 10:09:00 +0900

    #2.7 Location(+ Dog API)

--- App.js
+++ App.js
@@ -1,35 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+import * as Location from 'expo-location';
+import { StyleSheet, Text, View, ScrollView, Dimensions, Image } from 'react-native';
+// import axios from 'axios';
 
+// const {height:SCREEN_HEIGHT} = Dimensions.get('window');
 const {width:SCREEN_WIDTH} = Dimensions.get('window');
-
 export default function App() {
+  // const [imageSrc, setImageSrc] = useState("");
+
+  // useEffect(() => {
+  //   axios.get("https://dog.ceo/api/breeds/image/random")
+  //     .then((response) => {
+  //       setImageSrc(response.data.message);
+  //       console.log("imageSrc : " + imageSrc);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error)
+  //     });
+  // }, [])
+  const [city, setCity] = useState("Loading...");
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+  const ask = async() => {
+    const {granted} = await Location.requestForegroundPermissionsAsync();
+    if(!granted){
+      setOk(false);
+    }
+    const {
+      coords: {latitude, longitude}, 
+    } = await Location.getCurrentPositionAsync({accuracy:5});
+    const location = await Location.reverseGeocodeAsync(
+      {latitude, longitude}, 
+      {useGoogleMaps: false }
+      );
+      setCity(location[0].city)
+  }
+  useEffect(() => {
+    ask();
+  })
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView 
       pagingEnabled 
       horizontal 
       showsHorizontalScrollIndicator={false}
       contentContainerStyle={styles.weather}>
+        {/* <View>
+          <Image
+          source={{ uri: imageSrc }}
+          style={styles.dayDog}
+          />
+        </View> */}
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
         <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
+          <Text style={styles.temp}>28</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
         <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
+          <Text style={styles.temp}>29</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
         <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
+          <Text style={styles.temp}>30</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
       </ScrollView>
@@ -53,6 +94,11 @@ const styles = StyleSheet.create({
     fontWeight: "500",
   },
   weather: {},
+  // dayDog: {
+  //   width: SCREEN_WIDTH,
+  //   alignItems: "center",
+  //   height: SCREEN_HEIGHT,
+  // },
   day: {
     width: SCREEN_WIDTH,
     alignItems: "center",
@@ -70,23 +116,17 @@ const styles = StyleSheet.create({
 });
 
 /*
-[Component]
-ScrollView : 스크롤을 할 수 있음
-  * 일반 style은 적용X, contentContainerStyle로 사용
-  * ScrollView에는 flex가 없음(flex 적용X)
-→ Props
-  - horizontal : 가로 스크롤
-  - pagingEnabled : 스크롤을 자유롭게 하지 못하게끔, 대신 페이지가 생성됨
-  - showsHorizontalScrollIndicator={false} : 가로 스크롤바 숨기기
-  - indicatorStyle : 스크롤바 스타일 변경 (★only iOS)
-*/
-
-/*
 [API]
-- Dimensions : 사용자 화면 크기를 얻을 수 있음
-  * const {height, width} = Dimensions.get('window');
-  * height 또는 width 둘 중 하나만 가져올 수 있음
-  * width:SCREEN_WIDTH 와 같이 변수 이름을 설정할 수 있음
-  * styles로 가져와서 사용할 칸에 width: SCREEN_WIDTH 적어주면 적용됨
-  * === const SCREEN_WIDTH = Dimensions.get('window').width;
+Location : 유저 위치 정보
+  * npx expo install expo-location
+→ Methods
+  - .requestPermissionsAsync() : 권한 요청(필수)
+  - .requestForegroundPermissionsAsync() : 권한 딱 한 번만 물어봄
+  - .getLastKnownPositionAsync() : 유저의 마지막 위치
+  - .getCurrentPositionAsync() : 유저의 현재 위치
+  - .watchPositionAsync() : 위치 관찰(이동해도 따라가며 확인o)
+  - .geocodeAsync() : 주소를 받아서 위도/경도로 변환
+  - .reverseGeocodeAsync() : 위도/경도를 이 function에 주면 도시, 구역 반환
+  - Geofencing Method = 유저가 특정 지역을 벗어났을 때 알려줌
+    → 유저가 leave or enter 했을 때 알려줌
 */
